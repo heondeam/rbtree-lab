c144f99587665ecdc8a47b188b926d4fe9bf6912
heondeam <aoao1023@gmail.com>
2023-05-10 14:25:31 +0000

    comment push

--- src/rbtree.c
+++ src/rbtree.c
@@ -45,20 +45,19 @@ void rbtree_delete_helper(node_t *nil, node_t *s) {
 
 /**
  * RB tree를 삭제한다.
- * @param t *t 삭제할 트리의 주소값
- * @return 아무것도 반환하지 않는다.
+ * @param t 삭제할 트리의 주소값
 */
 void delete_rbtree(rbtree *t) {
-  // 트리를 후위 순회하면서 메모리를 반환한다.
+  // 후위 순회하면서 메모리를 반환한다.
   rbtree_delete_helper(t -> nil, t->root);
 
   free(t);
 }
 
-
 /**
- * 
- * 
+ * x를 기준으로 left rotation을 수행한다.
+ * @param tree 트리
+ * @param x 기준 노드
 */
 void left_rotate(rbtree *tree, node_t *x) {
   node_t *y;
@@ -82,6 +81,12 @@ void left_rotate(rbtree *tree, node_t *x) {
   x -> parent = y;
 }
 
+/**
+ * x를 기준으로 right rotation을 수행한다.
+ * @param tree 트리
+ * @param x 기준 노드
+ * 
+*/
 void right_rotate(rbtree *tree, node_t *x) {
   node_t *y;
 
@@ -107,62 +112,89 @@ void right_rotate(rbtree *tree, node_t *x) {
 }
 
 /**
- * 
- * 
- * 
+ * 삽입 연산 과정 중 RBtree의 속성을 위배했을 경우 fix 수행
+ * @param t 트리
+ * @param z 새로 삽입 할 노드
 */
 void rbtree_insert_fixup(rbtree *t, node_t *z) {
   node_t *y;
   
   while (z -> parent -> color == RBTREE_RED) {
-    // z의 부모가 조부모의 왼쪽 서브 트리일 경우
     if (z -> parent == z -> parent -> parent -> left) {
+      // z의 부모노드가 할아버지 노드의 왼쪽 노드일 경우
       y = z -> parent -> parent -> right;
-      
-      // CASE 1 : 노드 z의 삼촌 y가 적색인 경우
+
       if (y -> color == RBTREE_RED) {
+        // z의 삼촌 y가 RED
         z -> parent -> color = RBTREE_BLACK;
         y -> color = RBTREE_BLACK;
         z -> parent -> parent -> color = RBTREE_RED;
         z = z -> parent -> parent;
-      }
-      // CASE 2 : z의 삼촌 y가 흑색이며의 z가 오른쪽 자식인 경우
-      else {
+      }else {
+        // z의 삼촌 y가 BLACK 
         if (z == z -> parent -> right) {
+          // 삽입한 노드가 오른쪽 자식일 때
           z = z -> parent;
           left_rotate(t, z);
         }
-        // CASE 3 : z의 삼촌 y가 흑색이며의 z가 오른쪽 자식인 경우
+
+        // 삽입한 노드가 왼쪽 자식일 때
         z -> parent -> color = RBTREE_BLACK;
         z -> parent -> parent -> color = RBTREE_RED;
         right_rotate(t, z -> parent -> parent);
       }
-    }
-    // z의 부모가 조부모의 왼쪽 서브 트리일 경우
-    else {
+    }else {
+      // z의 부모노드가 할아버지 노드의 오른쪽 노드일 경우
       y = z -> parent -> parent -> left;
 
-      // CASE 4 : 노드 z의 삼촌 y가 적색인 경우
       if (y -> color == RBTREE_RED) {
+        // z의 삼촌 y가 RED
         z -> parent -> color = RBTREE_BLACK;
         y -> color = RBTREE_BLACK;
         z -> parent -> parent -> color = RBTREE_RED;
         z = z -> parent -> parent;
-      }
-      // CASE 5 : z의 삼촌 y가 흑색이며의 z가 오른쪽 자식인 경우
-      else {
+      }else {
+
         if (z == z -> parent -> left) {
+          // 삽입한 노드가 오른쪽 자식일 때
           z = z -> parent;
           right_rotate(t, z);
         }
-        // CASE 6 : z의 삼촌 y가 흑색이며의 z가 오른쪽 자식인 경우
+
+        // 삽입한 노드가 왼쪽 자식일 때
         z -> parent -> color = RBTREE_BLACK;
         z -> parent -> parent -> color = RBTREE_RED;
         left_rotate(t, z -> parent -> parent);
       }
     }
   }
+
+  // 루트는 항상 검은색.
   t -> root -> color = RBTREE_BLACK;
+}
+
+/**
+ * 이진 탐색 트리의 삽입 규칙을 지키면서 insert
+ * @param n *n 해당 노드의 주소값
+ * @param key 새로운 노드의 key
+ * @return 마지막 노드의 주소값
+*/
+node_t *path_find(rbtree *t, node_t *n, key_t key) {
+  int flag = (key <= n -> key) ? 0 : 1;
+
+  if (flag) {
+    // 새로운 노드의 키값이 현재 노드의 키값보다 크면? 오른쪽으로 들어감.
+    // 해당 노드의 오른쪽 노드가 비어있다면 현재 노드의 주소값 반환
+    // 비어있지 않다면 오른쪽으로 들어감.
+
+    return n -> right == t -> nil  ? n : path_find(t, n -> right, key);
+  }else {
+    // 새로운 노드의 키값이 현재 노드의 키값보다 작으면? 왼쪽으로 들어감.
+    // 해당 노드의 왼쪽 노드가 비어있다면 현재 노드의 주소값 반환
+    // 비어있지 않다면 왼쪽으로 들어감.
+
+    return n -> left == t -> nil ? n : path_find(t, n -> left, key);
+  }
 }
 
 /**
@@ -176,10 +208,14 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
   node_t *y = t -> nil;
   node_t *x = t -> root;
+
+  // 새로 추가할 노드의 메모리 할당.
   node_t *z = (node_t *)calloc(1, sizeof(node_t));
   
+  // 새로 추가할 노드의 key 바인딩.
   z -> key = key;
 
+  // 이진탐색트리의 삽입 연산 수행
   while (x != t -> nil) {
     y = x;
     if (z -> key < x -> key)
@@ -189,14 +225,14 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   }
 
   z -> parent = y;
-  
+
+  printf("%d ", y == t->nil ? 1 : y->key);
+
   if (y == t -> nil) {
     t -> root = z;
-  }
-  else if (z -> key < y -> key) {
+  }else if(z -> key < y -> key) {
     y -> left = z;
-  }
-  else {
+  }else {
     y -> right = z;
   }
 
@@ -350,6 +386,9 @@ void delete_fixup(rbtree *t, node_t *x) {
       node_t *w = x -> parent -> right;
       
       if(w -> color == RBTREE_RED) {
+        // x의 형제 w가 red
+        // x의 형제 w를 black으로 바꾼다 (부모와 형제의 색 교환 -> 부모 기준 왼쪽으로 회전.) 
+        // case 중 하나로 해결 (if문 이후에서 case 판별)
         w -> color = RBTREE_BLACK;
         x -> parent -> color = RBTREE_RED;
         left_rotate(t, x -> parent);
@@ -357,17 +396,22 @@ void delete_fixup(rbtree *t, node_t *x) {
       }
 
       if(w -> left -> color == RBTREE_BLACK && w -> right -> color == RBTREE_BLACK) {
+        // x의 형제 w의 왼쪽 자녀가 black & 오른쪽 자녀가 black
+        // doubly black과 w를 모아서 부모에게 전달하여 처리 위임
         w -> color = RBTREE_RED;
         x = x -> parent;
       }else { 
-        
         if (w -> right -> color == RBTREE_BLACK) {
+          // x의 형제 w의 왼쪽 자녀가 red & 오른쪽 자녀가 black
           w -> left -> color = RBTREE_BLACK;
           w -> color = RBTREE_RED;
           right_rotate(t, w);
           w = x -> parent -> right;
         }
 
+        // x의 형제 w의 오른쪽 자녀가 red
+        // doubly black의 부모를 red로 변경 후 extra black 전달 
+        // 후에 black으로 변경
         w -> color = x -> parent -> color;
         x -> parent -> color = RBTREE_BLACK;
         w -> right -> color = RBTREE_BLACK;
@@ -376,6 +420,7 @@ void delete_fixup(rbtree *t, node_t *x) {
       }
     }
     else {
+      // 왼쪽 형제 기준 즉, 반대 경우
       node_t *w = x -> parent -> left;
 
       if (w -> color == RBTREE_RED) {
@@ -421,8 +466,11 @@ int rbtree_erase(rbtree *t, node_t *p) {
   color_t org_color;
 
   y = p;
+  // 삭제되는 색 저장
+  // 삭제되는 색 1. 자녀가 없을떄: 삭제되는 노드의 색. 2. 자녀가 하나일 때: 삭제되는 노드의 색. 3. 자녀가 둘일때: successor의 색.
   org_color = y -> color;
 
+  // 이진탐색트리의 삭제 연산을 진행.
   if (p -> left == t -> nil) {
     x = p -> right;
     trans_plant(t, p, p -> right);
@@ -454,6 +502,8 @@ int rbtree_erase(rbtree *t, node_t *p) {
     y -> color = p -> color;
   }
 
+  // 삭제되는 색이 black일 때 속성 위반(임의의 노드에서 말단 노드까지 가는 경로에 포함된 black 노드의 수는 같다.)
+  // fixup 진행
   if (org_color == RBTREE_BLACK) {
     delete_fixup(t, x);
   }
@@ -461,4 +511,16 @@ int rbtree_erase(rbtree *t, node_t *p) {
   free(p);
 
   return 0;
+}
+
+
+
+int main() {
+
+  rbtree *t = new_rbtree();
+
+  rbtree_insert(t, 10);
+  rbtree_insert(t, 98);
+  rbtree_insert(t, 44);
+
 }
